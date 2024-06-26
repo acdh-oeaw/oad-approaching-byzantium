4d6f9eb6971f44d66d8edfa12135140efa5ba193
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-06-26 17:10:38 +0200

    fix: locale tests for trailing slashes

--- e2e/tests/app/metadata.test.ts
+++ e2e/tests/app/metadata.test.ts
@@ -2,6 +2,7 @@ import { createUrl } from "@acdh-oeaw/lib";
 
 import { locales } from "@/config/i18n.config";
 import { expect, test } from "@/e2e/lib/test";
+import { ensureTrailingSlash } from "@/utils/ensure-trailing-slash";
 import { escape } from "@/utils/safe-json-ld-replacer";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -12,10 +13,9 @@ test("should set a canonical url", async ({ createIndexPage }) => {
 		const { indexPage } = await createIndexPage(locale);
 		await indexPage.goto();
 
-		const canonicalUrl = indexPage.page.locator('link[rel="canonical"]');
-		await expect(canonicalUrl).toHaveAttribute(
-			"href",
-			String(createUrl({ baseUrl, pathname: `/${locale}` })),
+		const canonicalUrl = await indexPage.page.locator('link[rel="canonical"]').getAttribute("href");
+		expect(ensureTrailingSlash(String(canonicalUrl))).toBe(
+			String(createUrl({ baseUrl, pathname: `/` })),
 		);
 	}
 });
@@ -35,7 +35,7 @@ test("should set document title on not-found page", async ({ createI18n, page })
 });
 
 test("should disallow indexing of not-found page", async ({ page }) => {
-	for (const pathname of ["/unknown", "/de/unknown"]) {
+	for (const pathname of ["/unknown"]) {
 		await page.goto(pathname);
 
 		const ogTitle = page.locator('meta[name="robots"]');
@@ -80,11 +80,8 @@ test("should set page metadata", async ({ createIndexPage }) => {
 		const ogDescription = page.locator('meta[property="og:description"]');
 		await expect(ogDescription).toHaveAttribute("content", i18n.t("Metadata.description"));
 
-		const ogUrl = page.locator('meta[property="og:url"]');
-		await expect(ogUrl).toHaveAttribute(
-			"content",
-			String(createUrl({ baseUrl, pathname: `/${locale}` })),
-		);
+		const ogUrl = await page.locator('meta[property="og:url"]').getAttribute("content");
+		expect(ensureTrailingSlash(String(ogUrl))).toBe(String(createUrl({ baseUrl, pathname: `/` })));
 
 		const ogLocale = page.locator('meta[property="og:locale"]');
 		await expect(ogLocale).toHaveAttribute("content", locale);
